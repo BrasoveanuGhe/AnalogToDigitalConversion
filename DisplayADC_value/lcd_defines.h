f5cdf1e00526dcc041f21f19eef542ef945af616
Brasoveanu Gheorghita <51229567+BrasoveanuGhe@users.noreply.github.com>
2019-06-02 13:19:15 +0300

    Update lcd_defines.h

--- DisplayADC_value/lcd_defines.h
+++ DisplayADC_value/lcd_defines.h
@@ -22,22 +22,22 @@ volatile char *str;
 char displayFloatNum[20];
 
 
-/**************************************************************/
-/*			List of commonly used LCD Commands			      */
-/**************************************************************/
-#define LCD_CLEAR						0x01     
+/**************************************************************
+		List of commonly used LCD Commands
+**************************************************************/
+#define LCD_CLEAR				0x01     
  
-#define LCD_RETURN_HOME					0x02
+#define LCD_RETURN_HOME				0x02
 
-#define LCD_INCREMENT_RIGHT				0x06
-#define LCD_DECREMENT_LEFT				0x04
+#define LCD_INCREMENT_RIGHT			0x06
+#define LCD_DECREMENT_LEFT			0x04
 
 #define LCD_DISPLAY_ON_CURSOR_OFF		0x0C    
 #define LCD_DISPLAY_ON_CURSOR_ON		0x0E     
 #define LCD_DISPLAY_ON_CURSOR_BLINK		0x0F  
 
-#define LCD_FOUR_BIT_MODE				0x28	       
-#define LCD_EIGHT_BIT_MODE				0x38 // 8 bits; 2 lines; 5x8 dots (character size)
+#define LCD_FOUR_BIT_MODE			0x28	       
+#define LCD_EIGHT_BIT_MODE			0x38 // 8 bits; 2 lines; 5x8 dots (character size)
 
 #define LCD_CURSOR_HOME_POSITION		0x80
 
@@ -56,19 +56,6 @@ typedef struct
 lcd_commandInit lcd_portInit;
 
 
-//            DE COMPLETAT
-
-/**
- * \brief 
- * 
- * \param dataPort
- * \param cmdPort
- * \param rsPin
- * \param rwPin
- * \param enPin
- * 
- * \return void
- */
 void liquidCrystalPortInit(volatile uint8_t *dataPort, volatile  uint8_t *cmdPort, uint8_t rsPin, uint8_t rwPin, uint8_t enPin);
 
 
